3bfe4dde1fe6ad81d14df94a93b64a6461e73d59
Khoullins <47117632+Khoullins@users.noreply.github.com>
2019-02-07 19:21:13 +0300

    My first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the base and height of your triangle.");
+            Console.WriteLine("Enter base");
+            int bas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter height");
+            int height = Convert.ToInt32(Console.ReadLine());
+            double area = 0.5 * bas * height;
+            Console.Clear();
+            Console.WriteLine("Base = "+bas);
+            Console.WriteLine("Height = "+height);
+            Console.WriteLine( ""+ 
+                $"The area is {area} metres squared");
+            Console.ReadKey();
+
+        }
+    }
+}
